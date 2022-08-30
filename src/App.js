a0ba57c9a4b425d2ae4d5a4c0bba37b4b88ae71d
fridavbg <fperssontech@gmail.com>
2022-08-30 18:21:50 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import "./styles/app.scss";
 import Header from "./components/incl/Header";
 import Editor from "./components/Editor";
 
-import Footer from "./components/incl/Footer";
-
 function App() {
     return (
         <div className="App">
             <Header />
             <div>
-                {/* <Editor /> */}
-                <div className="parent">
-                    <div className="child" />
-                </div>
+                <Editor />
             </div>
-            <Footer />
         </div>
     );
 }
--- src/components/incl/Header.js
+++ src/components/incl/Header.js
@@ -1,5 +1,9 @@
 function Header() {
-    return <div className="Header">Header</div>;
+    return (
+        <div className="header">
+            <h1>React Text Editor</h1>
+        </div>
+    );
 }
 
 export default Header;
