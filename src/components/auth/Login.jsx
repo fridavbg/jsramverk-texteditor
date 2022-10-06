a126f78f7edfae69088254b78036bd1d657f449a
fridavbg <fperssontech@gmail.com>
2022-10-06 17:01:04 +0200

    login form styling

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
+import authModel from "../../models/auth"
+
 function Login() {
+    const [user, setUser] = useState({});
+
+    function changeHandler(event) {
+        let newObject = {};
+
+        newObject[event.target.name] = event.target.value;
+
+        setUser({ ...user, ...newObject });
+    }
+
+    async function register() {
+        await authModel.register(user);
+    }
+
+    async function login() {
+        await authModel.login(user);
+    }
+
     return (
-        <div className="title">
-            <h1>Login / Registrera</h1>
+        <div className="login-form">
+            <h1 className="title">Login / Registrera</h1>
+            <input type="email" name="email" onChange={changeHandler} />
+            <input type="password" name="password" onChange={changeHandler} />
+            <button onClick={register}>Registrera</button>
+            <button onClick={login}>Logga in</button>
         </div>
     );
 }
--- src/models/auth.jsx
+++ src/models/auth.jsx
@@ -1,5 +1,7 @@
 const auth = {
     token: "",
+    login: async function login() {},
+    register: async function register() {},
 };
 
 export default auth;
