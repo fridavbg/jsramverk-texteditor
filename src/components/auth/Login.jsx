f735025153d76c799c6d140dc4e4dba40c2648a3
fridavbg <fperssontech@gmail.com>
2022-10-06 20:04:26 +0200

    Login Form validation

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -6,19 +6,29 @@ import authModel from "../../models/auth";
 function Login({ setToken }) {
     const navigate = useNavigate();
     const [user, setUser] = useState({});
+    const [error, setError] = useState("");
+    const [message, setMessage] = useState("");
 
     function changeHandler(event) {
         let newObject = {};
 
         newObject[event.target.name] = event.target.value;
-
         setUser({ ...user, ...newObject });
     }
 
     async function register() {
+        setError("");
+        setMessage("");
         const registerResult = await authModel.register(user);
 
-        console.log("register", registerResult.data);
+        console.log("register", registerResult.errors);
+
+        if (registerResult.errors !== undefined) {
+            setError(registerResult.errors.message);
+        }
+        if (registerResult.data.message !== undefined) {
+            setMessage(registerResult.data.message);
+        }
 
         if (registerResult.data.token) {
             setToken(registerResult.data.token);
@@ -26,9 +36,19 @@ function Login({ setToken }) {
     }
 
     async function login() {
+        setError("");
+        setMessage("");
         const loginResult = await authModel.login(user);
 
         console.log("login:", loginResult.data);
+
+        if (loginResult.errors !== undefined) {
+            setError(loginResult.errors.message);
+        }
+
+        if (loginResult.data !== undefined) {
+            setMessage(loginResult.data.message);
+        }
 
         if (loginResult.data.token) {
             setToken(loginResult.data.token);
@@ -36,17 +56,14 @@ function Login({ setToken }) {
         }
     }
 
-    return (
-        <>
-            <h1 className="title">Login / Registrera</h1>
-            <form className="login-form">
+    if (error || message) {
+        return (
+            <div className="login-form">
+                <h1 className="title">Login / Registrera</h1>
+                <p class="err-msg">{error || message}</p>
+
                 <label>Email:</label>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={changeHandler}
-                    required
-                />
+                <input type="email" name="email" onChange={changeHandler} />
                 <label>Password:</label>
                 <input
                     type="password"
@@ -54,10 +71,35 @@ function Login({ setToken }) {
                     onChange={changeHandler}
                     required
                 />
-                <button onClick={register}>Registrera</button>
-                <button onClick={login}>Logga in</button>
-            </form>
-        </>
+                <button className="register-btn" onClick={register}>
+                    Registrera
+                </button>
+                <button className="login-btn" onClick={login}>
+                    Logga in
+                </button>
+            </div>
+        );
+    }
+    return (
+        <div className="login-form">
+            <h1 className="title">Login / Registrera</h1>
+
+            <label>Email:</label>
+            <input type="email" name="email" onChange={changeHandler} />
+            <label>Password:</label>
+            <input
+                type="password"
+                name="password"
+                onChange={changeHandler}
+                required
+            />
+            <button className="register-btn" onClick={register}>
+                Registrera
+            </button>
+            <button className="login-btn" onClick={login}>
+                Logga in
+            </button>
+        </div>
     );
 }
 
